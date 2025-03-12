e92f8617b6ba03953eb428376526e2a43a9b1ba3
Pranav Chandane <pranavhchandane@gmail.com>
2025-03-12 21:23:17 +0530

    users now need to provide their own API key

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import streamlit as st
 from google import genai
 from google.genai import types
-import os
-from dotenv import load_dotenv; load_dotenv()
-
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 st.set_page_config(
     page_title="Promptopia",
@@ -14,41 +10,51 @@ st.set_page_config(
 st.title("🚀 Promptopia")
 st.subheader("Test your prompt engineering skills!")
 
-user_prompt = st.text_area("Enter your prompt:", "", placeholder="The prompt is the text that the model uses to generate a response. Use the following controls over the hyperparameters of the Gemini 2.0 Flash LLM to customize the response. Good luck!")
+user_api_key:str = st.text_input("Enter your Gemini API Key:", placeholder="You cannot proceed without a valid API key", type="password")
 
-# Hyperparameter Controls
-temperature = st.slider("Temperature", 0.0, 2.0, 1.0, 0.01)
-top_k = st.slider("Top-K Sampling", 0, 5000, 100, 1)
-top_p = st.slider("Top-P (Nucleus Sampling)", 0.0, 1.0, 0.95, 0.01)
-max_output_tokens = st.slider("Max Tokens", 1, 10000, 200, 1)
+if user_api_key:
+    try:
+        client = genai.Client(api_key=user_api_key)
 
-if st.button("Generate Response"):
-    st.subheader("LLM Output")
-    stream = st.empty()
-    stream.write("Generating response... 🚀")
-    full_response = ""
+        user_prompt:str = st.text_area("Enter your prompt:", "", placeholder="The prompt is the text that the model uses to generate a response. Use the following controls over the hyperparameters of the Gemini 2.0 Flash LLM to customize the response. Good luck!")
 
-    if user_prompt.strip():
-        response = client.models.generate_content_stream(
-            model="gemini-2.0-flash",
-            contents=user_prompt,
-            config= types.GenerateContentConfig(
-                max_output_tokens=max_output_tokens,
-                temperature=temperature,
-                top_k=top_k,
-                top_p=top_p
-            )
-        )
-        print(max_output_tokens, temperature, top_k, top_p)
-        for chunk in response:
-            if chunk.text: 
-                full_response += chunk.text
-                stream.write(full_response)
+        # Hyperparameter Controls
+        st.markdown("**Warning: Setting extreme values might break the response!**")
+        temperature = st.slider("Temperature", 0.0, 2.0, 1.0, 0.01)
+        top_k = st.slider("Top-K Sampling", 0, 5000, 100, 1)
+        top_p = st.slider("Top-P (Nucleus Sampling)", 0.0, 1.0, 0.95, 0.01)
+        max_output_tokens = st.slider("Max Tokens", 1, 10000, 200, 1)
+
+        if st.button("Generate Response"):
+            st.subheader("LLM Output")
+            stream = st.empty()
+            stream.write("Generating response... 🚀")
+            full_response = ""
+
+            if user_prompt.strip():
+                response = client.models.generate_content_stream(
+                    model="gemini-2.0-flash",
+                    contents=user_prompt,
+                    config= types.GenerateContentConfig(
+                        max_output_tokens=max_output_tokens,
+                        temperature=temperature,
+                        top_k=top_k,
+                        top_p=top_p
+                    )
+                )
+                print(max_output_tokens, temperature, top_k, top_p)
+                for chunk in response:
+                    if chunk.text: 
+                        full_response += chunk.text
+                        stream.write(full_response)
+                    else:
+                        stream.write("No response generated. Please try again.")   
             else:
-                stream.write("No response generated. Please try again.")
-        
-    else:
-        st.warning("Please enter a prompt!")
+                st.warning("Please enter a prompt!")
+    except Exception as e:
+        st.error("Invalid API Key. Please try again.")
+else:
+    st.warning("Generate a free Gemini API Key: https://aistudio.google.com/apikey")
 
 st.markdown("---")
 st.markdown("❤️ Made using Streamlit and Gemini 2.0 Flash")
